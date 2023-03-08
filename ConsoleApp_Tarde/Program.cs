ef72eba3978a264327ef4582dc6f7aedcfd99dd1
EduardoBelardinuci <belardinucieduardo@gmail.com>
2023-03-08 16:06:55 -0300

    Criação da Classe Cliente e Endereco, criação do método do Construtor e dos métodos Get e Set. Alterando o Program para exibição dos itens.

--- ConsoleApp_Tarde/Program.cs
+++ ConsoleApp_Tarde/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleApp_Tarde;
 
-
-
 Produto p1 = new Produto(1, "Mouse Xing Ling", 10.90M);
 Produto p2 = new Produto(2, "Teclado Multilaser", 20.90M);
 
@@ -13,5 +11,26 @@ p1.setDescricao("Mouse Razer");
 
 Console.WriteLine(p1.getDescricao() + " " + p1.getValor());
 
+//Classe Cliente
+
+Cliente c1 = new Cliente(1, "Eduardo", "16981023552");
+
+Console.WriteLine(c1.getNome() + " " + c1.getTelefone());
+
+c1.setNome("Joao");
+c1.setTelefone("16981003461");
+
+Console.WriteLine(c1.getNome() + " " + c1.getTelefone());
+
+//Classe Endereco
+
+Endereco e1 = new Endereco(1, "15906-044", "Rua Franciso", 93, "Centro", "Taquaritinga");
+
+Console.WriteLine(e1.getId() + " " + e1.getCEP() + " " + e1.getCidade() + " " + e1.getBairro());
+
+e1.setCidade("Araraquara");
+e1.setBairro("Vila");
+
+Console.WriteLine(e1.getCidade() + " " + e1.getBairro());
 
 
